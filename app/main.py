624e4c1b3672114f09c8201f5aeefad145fb02db
Stepan Kuzin <stepankuzinmail@gmail.com>
2021-11-02 00:21:38 +0500

    Add volumes to docker and fix imports

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
 import sentry_sdk
 from fastapi import FastAPI
 from sentry_sdk.integrations.asgi import SentryAsgiMiddleware
+from settings.settings import settings
 
 from app.api.v1.router import api_router
-from app.core.config import settings
 
-app = FastAPI(title=settings.PROJECT_NAME, openapi_url="/v1/openapi.json")
+app = FastAPI(title=settings.API_NAME, openapi_url="/v1/openapi.json")
 
 if settings.SENTRY_DSN:
     sentry_sdk.init(dsn=settings.SENTRY_DSN)
--- app/settings/settings.py
+++ app/settings/settings.py
@@ -4,6 +4,7 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     API_NAME: str = "Music API"
     POSTGRES_URL: str = "postgresql://postgres:postgres@postgres:5432"
+    SENTRY_DSN: str = ""
 
 
 settings = Settings()
